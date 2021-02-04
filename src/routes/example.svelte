d765b6d60910b9a3e3b3be5dbcd69847d7284dd0
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-02-04 00:53:43 -0800

    added svelte-pdf, doesn't crash, but doesn't display either

--- src/routes/example.svelte
+++ src/routes/example.svelte
@@ -1,3 +1,15 @@
+<script>
+    import { onMount } from "svelte";
+    let PdfViewer;
+
+    onMount(async () => {
+        const module = await import("svelte-pdf");
+        PdfViewer = module.default;
+    });
+</script>
+
+<svelte:component this={PdfViewer} url="./pdfs/pleasemarkdarkly.pdf" />
+
 <div class="flex items-center justify-center px-16 py-16">
     <div class="bg-red-500 w-10 h-10" />
     <div class="bg-blue-500 w-10 h-10" />
